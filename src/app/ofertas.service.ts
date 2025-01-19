c66072c49f2f5cf5b12792840b3708481c1950aa
Thiagoferlima <th1gox2003@gmail.com>
2025-01-19 11:49:22 -0300

    HTTpAPIsRest

--- src/app/ofertas.service.ts
+++ src/app/ofertas.service.ts
@@ -1,81 +1,14 @@
 import { HttpClient } from "@angular/common/http"
 import { Oferta } from "./shared/ofertas.model"
 import { Injectable } from "@angular/core"
+import { lastValueFrom } from 'rxjs';
 @Injectable()
 export class OfertasService{
     
     constructor(private http: HttpClient) {}
 
-     public ofertas : Oferta[] =    [
-            {
-                id: 1,
-                categoria: "restaurante",
-                titulo: "Super Burger",
-                descricao_oferta: "Rodízio de Mini-hambúrger com opção de entrada.",
-                anunciante: "Original Burger",
-                valor: 29.90,
-                destaque: true,
-                imagens: [
-                    {url: "/assets/ofertas/1/img1.jpg"},
-                    {url: "/assets/ofertas/1/img2.jpg"},
-                    {url: "/assets/ofertas/1/img3.jpg"},
-                    {url: "/assets/ofertas/1/img4.jpg"}
-                ]
-            },
-            {
-                id: 2,
-                categoria: "restaurante",
-                titulo: "Cozinha Mexicana",
-                descricao_oferta: "Almoço ou Jantar com Rodízio Mexicano delicioso.",
-                anunciante: "Mexicana",
-                valor: 32.90,
-                destaque: true,
-                imagens: [
-                    {url: "/assets/ofertas/2/img1.jpg"},
-                    {url: "/assets/ofertas/2/img2.jpg"},
-                    {url: "/assets/ofertas/2/img3.jpg"},
-                    {url: "/assets/ofertas/2/img4.jpg"}
-                ]
-            
-            },
-            {
-                id: 4,
-                categoria: "diversao",
-                titulo: "Estância das águas",
-                descricao_oferta: "Diversão garantida com piscinas, trilhas e muito mais.",
-                anunciante: "Estância das águas",
-                valor: 31.90,
-                destaque: true,
-                imagens: [
-                    {url: "/assets/ofertas/3/img1.jpg"},
-                    {url: "/assets/ofertas/3/img2.jpg"},
-                    {url: "/assets/ofertas/3/img3.jpg"},
-                    {url: "/assets/ofertas/3/img4.jpg"},
-                    {url: "/assets/ofertas/3/img5.jpg"},
-                    {url: "/assets/ofertas/3/img6.jpg"}
-                ]
-            }
-        ]
+    public getOfertas(): Promise<Oferta[]> {
+        return lastValueFrom(this.http.get<Oferta[]>('http://localhost:3000/ofertas'));
+      }
     
-
-    public getOfertas(): Array<Oferta>{
-        return this.ofertas
-    }
-    public getOfertas2(): Promise<Oferta[]> {
-        return new Promise((resolve,reject) => {
-            let deu_certo = true
-
-            if(deu_certo){
-                setTimeout(() => resolve(this.ofertas), 3000)
-            }
-            else{
-                reject({codigo_erro: 404, mensagem_erro:'servidor não encontrado'})
-            }
-            
-        })
-        .then((ofertas:Oferta[]) => {
-            console.log('primeiro then')
-            return ofertas
-        })
-    }
 }
