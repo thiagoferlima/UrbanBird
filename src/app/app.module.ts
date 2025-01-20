95e90981eac7d769833dca820ca1fcc8b20e5406
Thiagoferlima <th1gox2003@gmail.com>
2025-01-19 22:04:46 -0300

    RoutesMap

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,7 +1,8 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import {HttpClientModule} from '@angular/common/http'
-
+import { RouterModule } from '@angular/router';
+import { ROUTES } from './app.routes';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -23,7 +24,9 @@ import { DiversaoComponent } from './diversao/diversao.component';
   imports: [
     BrowserModule,
     AppRoutingModule,
-    HttpClientModule
+    HttpClientModule, 
+    RouterModule.forRoot(ROUTES)
+
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/app.routes.ts
@@ -0,0 +1,18 @@
+import { Routes } from "@angular/router";
+
+import { HomeComponent } from "./home/home.component";
+
+import { RestaurantesComponent } from "./restaurantes/restaurantes.component";
+import { DiversaoComponent } from "./diversao/diversao.component";
+
+export const ROUTES: Routes = [
+    {
+       path: '', component: HomeComponent 
+    },
+    {
+        path: 'restaurantes', component: HomeComponent
+    },
+    {
+        path: 'diversao', component: DiversaoComponent
+    },
+]
