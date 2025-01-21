351fe72c7c9a5ee4e3b6e29dfe1fab067eebc2e2
Thiagoferlima <th1gox2003@gmail.com>
2025-01-21 15:30:50 -0300

    CreatingAndNavigatingToTheOfferPage

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,12 +1,13 @@
 import { Routes } from "@angular/router";
 
 import { HomeComponent } from "./home/home.component";
-
+import { OfertaComponent } from "./oferta/oferta.component";
 import { RestaurantesComponent } from "./restaurantes/restaurantes.component";
 import { DiversaoComponent } from "./diversao/diversao.component";
 
 export const ROUTES: Routes = [
     {path: '', component: HomeComponent },
     {path: 'restaurantes', component: RestaurantesComponent},
-    {path: 'diversao', component: DiversaoComponent}
+    {path: 'diversao', component: DiversaoComponent},
+    {path: 'oferta', component: OfertaComponent}
 ]
