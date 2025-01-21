df31ff619f0a5a5266fdd70fb407937058b40ada
Thiagoferlima <th1gox2003@gmail.com>
2025-01-21 10:52:31 -0300

    RestaurantOffers

--- src/app/ofertas.service.ts
+++ src/app/ofertas.service.ts
@@ -1,14 +1,21 @@
 import { HttpClient } from "@angular/common/http"
 import { Oferta } from "./shared/ofertas.model"
 import { Injectable } from "@angular/core"
-import { lastValueFrom } from 'rxjs';
 @Injectable()
 export class OfertasService{
     
     constructor(private http: HttpClient) {}
 
     public getOfertas(): Promise<Oferta[]> {
-        return lastValueFrom(this.http.get<Oferta[]>('http://localhost:3000/ofertas?destaque=true'));
+        return this.http.get('http://localhost:3000/ofertas?destaque=true')
+               .toPromise()
+               .then((resposta:any) => resposta.json())
+            
       }
+    public getOfertasPorCategoria(categoria: string) : Promise<Oferta[]>{
+      return this.http.get(`http://localhost:3000/ofertas?categoria=${categoria}`)
+      .toPromise()
+      .then((resposta:any) => resposta.json())
+    }
     
 }
