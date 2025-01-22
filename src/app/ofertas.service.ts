6a4092ad8b3a12900a8fb53268f6f60b9edde0fa
Thiagoferlima <th1gox2003@gmail.com>
2025-01-22 11:33:58 -0300

    APIurl

--- src/app/ofertas.service.ts
+++ src/app/ofertas.service.ts
@@ -4,28 +4,32 @@ import { Injectable } from "@angular/core"
 import { promise } from "protractor";
 @Injectable()
 export class OfertasService{
+
+  private url_api = 'http://localhost:3000/ofertas'
     
     constructor(private http: HttpClient) {}
 
     public getOfertas(): Promise<Oferta[]> {
       return this.http
-        .get<Oferta[]>('http://localhost:3000/ofertas?destaque=true') // Adicione o tipo Oferta[]
+        .get<Oferta[]>(`${this.url_api}?destaque=true`) // Adicione o tipo Oferta[]
         .toPromise(); // Não precisa de then para resposta.json()
     }
   
     public getOfertasPorCategoria(categoria: string): Promise<Oferta[]> {
       return this.http
-        .get<Oferta[]>(`http://localhost:3000/ofertas?categoria=${categoria}`)
+        .get<Oferta[]>(`${this.url_api}?categoria=${categoria}`)
         .toPromise();
     }
 
-    public getOfertasPorId(id: number): Promise<Oferta> {
-      return this.http.get<Oferta[]>(`http://localhost:3000/ofertas?id=${id}`)
-        .toPromise()
-        .then((resposta: Oferta[]) => {
-          return resposta[0]; 
-        })
-    }
+public getOfertasPorId(id: number): Promise<Oferta> {
+  return this.http.get<Oferta[]>(`${this.url_api}?id=${id}`)
+    .toPromise()
+    .then((resposta: Oferta[]) => {
+      // Verifique se a resposta contém dados e se está retornando a oferta certa
+      return resposta[0]; 
+    });
+}
+
     
     
 }
