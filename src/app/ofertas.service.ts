f5f8aadacddc5ef25bb34a1f419ff64b56945ff5
Thiagoferlima <th1gox2003@gmail.com>
2025-01-22 14:36:11 -0300

    AcessAPIurl

--- src/app/ofertas.service.ts
+++ src/app/ofertas.service.ts
@@ -2,27 +2,29 @@ import { HttpClient } from "@angular/common/http"
 import { Oferta } from "./shared/ofertas.model"
 import { Injectable } from "@angular/core"
 import { promise } from "protractor";
+import { URL_API } from "./app.api";
+
 @Injectable()
 export class OfertasService{
 
-  private url_api = 'http://localhost:3000/ofertas'
+ // private url_api = 'http://localhost:3000/ofertas'
     
     constructor(private http: HttpClient) {}
 
     public getOfertas(): Promise<Oferta[]> {
       return this.http
-        .get<Oferta[]>(`${this.url_api}?destaque=true`) // Adicione o tipo Oferta[]
+        .get<Oferta[]>(`${URL_API}?destaque=true`) // Adicione o tipo Oferta[]
         .toPromise(); // Não precisa de then para resposta.json()
     }
   
     public getOfertasPorCategoria(categoria: string): Promise<Oferta[]> {
       return this.http
-        .get<Oferta[]>(`${this.url_api}?categoria=${categoria}`)
+        .get<Oferta[]>(`${URL_API}?categoria=${categoria}`)
         .toPromise();
     }
 
 public getOfertasPorId(id: number): Promise<Oferta> {
-  return this.http.get<Oferta[]>(`${this.url_api}?id=${id}`)
+  return this.http.get<Oferta[]>(`${URL_API}?id=${id}`)
     .toPromise()
     .then((resposta: Oferta[]) => {
       // Verifique se a resposta contém dados e se está retornando a oferta certa
