0977b9882ebd6315d659bc00eaeb5d4cae7de681
Thiagoferlima <th1gox2003@gmail.com>
2025-01-25 14:35:23 -0300

    ObservablesMemoryLeakAndUnsubscribe

--- src/app/oferta/oferta.component.ts
+++ src/app/oferta/oferta.component.ts
@@ -1,16 +1,20 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, OnDestroy } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
 import { OfertasService } from '../ofertas.service';
 import { Oferta } from '../shared/ofertas.model';
 import { Observable } from 'rxjs';
 import 'rxjs/Rx'
+import { Observer } from 'rxjs';
+import { Subscription } from 'rxjs';
 @Component({
   selector: 'app-oferta',
   templateUrl: './oferta.component.html',
   styleUrls: ['./oferta.component.css'],
   providers: [OfertasService]
 })
-export class OfertaComponent implements OnInit {
+export class OfertaComponent implements OnInit, OnDestroy {
+  private tempoObservableSubscription: Subscription
+  private meuObservableTesteSubscription:Subscription
   public oferta:Oferta
   constructor(
     private route: ActivatedRoute, 
@@ -31,13 +35,35 @@ export class OfertaComponent implements OnInit {
     })
       */
 
+    
     let tempo = Observable.interval(2000)
 
-    tempo.subscribe((intervalo: number) =>{
+    this.tempoObservableSubscription = tempo.subscribe((intervalo: number) =>{
       console.log(intervalo)
 
     })
+      
 
+    let meuObservableTeste = Observable.create((observer: Observer<number>)=> {
+     observer.next(1)
+     observer.next(3)
+     observer.complete()
+     observer.next(3)
+
+
+    })
+
+
+
+     this.meuObservableTesteSubscription=meuObservableTeste.subscribe(
+      (resultado:number) => console.log(resultado + 10),
+      (erro:string) => console.log(erro),
+      () => console.log('Stream de eventos foi finalizada')
+    )
+  }
+  ngOnDestroy(): void {
+    this.meuObservableTesteSubscription.unsubscribe()
+    this.tempoObservableSubscription.unsubscribe()
   }
 
 
