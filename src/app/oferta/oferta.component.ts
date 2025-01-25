9e6dc652f8960d76f9fee5b6ba094387cd8e1115
Thiagoferlima <th1gox2003@gmail.com>
2025-01-25 14:59:03 -0300

    OfferSearchField

--- src/app/oferta/oferta.component.ts
+++ src/app/oferta/oferta.component.ts
@@ -2,10 +2,6 @@ import { Component, OnInit, OnDestroy } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
 import { OfertasService } from '../ofertas.service';
 import { Oferta } from '../shared/ofertas.model';
-import { Observable } from 'rxjs';
-import 'rxjs/Rx'
-import { Observer } from 'rxjs';
-import { Subscription } from 'rxjs';
 @Component({
   selector: 'app-oferta',
   templateUrl: './oferta.component.html',
@@ -13,8 +9,6 @@ import { Subscription } from 'rxjs';
   providers: [OfertasService]
 })
 export class OfertaComponent implements OnInit, OnDestroy {
-  private tempoObservableSubscription: Subscription
-  private meuObservableTesteSubscription:Subscription
   public oferta:Oferta
   constructor(
     private route: ActivatedRoute, 
@@ -27,43 +21,9 @@ export class OfertaComponent implements OnInit, OnDestroy {
       this.oferta = oferta
      
     })
-    /*
-    this.route.params.subscribe(
-      (parametro:any) => {console.log(parametro),
-      (erro: any) => console.log(erro),
-      () => console.log('processamento foi classificado como concluído!')
-    })
-      */
-
-    
-    let tempo = Observable.interval(2000)
-
-    this.tempoObservableSubscription = tempo.subscribe((intervalo: number) =>{
-      console.log(intervalo)
-
-    })
-      
-
-    let meuObservableTeste = Observable.create((observer: Observer<number>)=> {
-     observer.next(1)
-     observer.next(3)
-     observer.complete()
-     observer.next(3)
-
-
-    })
-
-
-
-     this.meuObservableTesteSubscription=meuObservableTeste.subscribe(
-      (resultado:number) => console.log(resultado + 10),
-      (erro:string) => console.log(erro),
-      () => console.log('Stream de eventos foi finalizada')
-    )
+ 
   }
   ngOnDestroy(): void {
-    this.meuObservableTesteSubscription.unsubscribe()
-    this.tempoObservableSubscription.unsubscribe()
   }
 
 
