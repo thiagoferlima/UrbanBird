18da43e476485ba6bc5044735b1ba049baad8a14
thiago-fernandes <thicodes.com>
2025-01-30 12:10:41 -0300

    Updating como-usar and onde-fica in the route change process

--- app2/src/app/oferta/como-usar/como-usar.component.ts
+++ app2/src/app/oferta/como-usar/como-usar.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { ActivatedRoute } from '@angular/router'
+import { ActivatedRoute, Params } from '@angular/router'
 import { OfertasService } from '../../ofertas.services'
 
 @Component({
@@ -17,11 +17,15 @@ export class ComoUsarComponent implements OnInit {
   ) { }
 
   ngOnInit() {
-    
-    this.ofertaServices.getComoUsarOfertaPorId(this.route.parent.snapshot.params['id'])
-    .then((descricao: string) => {
-      this.comoUsar = descricao
+
+    this.route.parent.params.subscribe((parametros: Params)=> {
+      this.ofertaServices.getComoUsarOfertaPorId(parametros.id)
+      .then((descricao: string) => {
+        this.comoUsar = descricao
+      })
     })
+    
+
   }
 
 }
