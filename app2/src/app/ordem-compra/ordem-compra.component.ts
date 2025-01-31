2a23e6bc51840eb5969a72fd2f7f4924f07d7186
thiago-fernandes <thicodes.com>
2025-01-31 12:00:14 -0300

    Validando dados do formulário

--- app2/src/app/ordem-compra/ordem-compra.component.ts
+++ app2/src/app/ordem-compra/ordem-compra.component.ts
@@ -13,27 +13,54 @@ export class OrdemCompraComponent implements OnInit {
   public formaPagamento: string=''
 
 
+  public enderecoValido: boolean
+  public numeroValido: boolean
+  public complementoValido: boolean
+  public formaPagamentoValido: boolean
+
+
   constructor() { }
 
   ngOnInit() {
   }
   public atulizaEndereco(endereco:string): void{
     this.endereco = endereco
-    console.log(this.endereco)
+    //console.log(this.endereco)
+    if(this.endereco.length > 3){
+      this.enderecoValido = true
+    }else{
+      this.enderecoValido= false
+    }
   }
   public atulizaNumero(numero:string):void{
     this.numero = numero
-    console.log(this.numero)
+    //console.log(this.numero)
+    if(this.numero.length> 0){
+      this.numeroValido = true
+    }else{
+      this.numeroValido = false
+    }
 
   }
   public atulizaComplemento(complemento: string): void{
     this.complemento = complemento
-    console.log(this.complemento)
+   // console.log(this.complemento)
+   if(this.complemento.length> 0){
+    this.complementoValido= true
+   }else{
+    this.complementoValido=false
+   }
 
   }
   public atualizaFormaPagamento(formaPagamento:string):void{
     this.formaPagamento = formaPagamento
-    console.log(this.formaPagamento)
+    //console.log(this.formaPagamento)
+    if(this.formaPagamento.length> 0){
+      this.formaPagamentoValido = true
+    }
+    else{
+      this.formaPagamentoValido = false
+    }
 
   }
 
