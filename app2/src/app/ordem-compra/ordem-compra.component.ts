e01a32a875fab5ec49a2d26832f4ccd21a112598
thiago-fernandes <thicodes.com>
2025-01-31 12:25:30 -0300

    Controlando estado primitivo dos campos

--- app2/src/app/ordem-compra/ordem-compra.component.ts
+++ app2/src/app/ordem-compra/ordem-compra.component.ts
@@ -19,6 +19,10 @@ export class OrdemCompraComponent implements OnInit {
   public formaPagamentoValido: boolean
 
 
+  public enderecoEstadoPrimitivo: boolean = true
+  public numeroEstadoPrimitivo: boolean = true
+  public complementoEstadoPrimitivo: boolean = true
+  public formaPagamentoEstadoPrimitivo:boolean=true
   constructor() { }
 
   ngOnInit() {
@@ -26,6 +30,7 @@ export class OrdemCompraComponent implements OnInit {
   public atulizaEndereco(endereco:string): void{
     this.endereco = endereco
     //console.log(this.endereco)
+    this.complementoEstadoPrimitivo = false
     if(this.endereco.length > 3){
       this.enderecoValido = true
     }else{
@@ -35,6 +40,8 @@ export class OrdemCompraComponent implements OnInit {
   public atulizaNumero(numero:string):void{
     this.numero = numero
     //console.log(this.numero)
+    this.numeroEstadoPrimitivo=false
+
     if(this.numero.length> 0){
       this.numeroValido = true
     }else{
@@ -45,6 +52,7 @@ export class OrdemCompraComponent implements OnInit {
   public atulizaComplemento(complemento: string): void{
     this.complemento = complemento
    // console.log(this.complemento)
+   this.complementoEstadoPrimitivo=false
    if(this.complemento.length> 0){
     this.complementoValido= true
    }else{
@@ -55,6 +63,7 @@ export class OrdemCompraComponent implements OnInit {
   public atualizaFormaPagamento(formaPagamento:string):void{
     this.formaPagamento = formaPagamento
     //console.log(this.formaPagamento)
+    this.formaPagamentoEstadoPrimitivo=false
     if(this.formaPagamento.length> 0){
       this.formaPagamentoValido = true
     }
