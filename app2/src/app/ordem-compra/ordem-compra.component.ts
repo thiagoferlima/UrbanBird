4b58fe62e5ea9c8fbc9ec49795c80f11efc5acde
thiago-fernandes <thicodes.com>
2025-01-31 10:49:44 -0300

    Two-Way-Binding dos dados do formulário

--- app2/src/app/ordem-compra/ordem-compra.component.ts
+++ app2/src/app/ordem-compra/ordem-compra.component.ts
@@ -7,9 +7,34 @@ import { Component, OnInit } from '@angular/core';
 })
 export class OrdemCompraComponent implements OnInit {
 
+  public endereco:string= ''
+  public numero: string = ''
+  public  complemento: string =''
+  public formaPagamento: string=''
+
+
   constructor() { }
 
   ngOnInit() {
+  }
+  public atulizaEndereco(endereco:string): void{
+    this.endereco = endereco
+    console.log(this.endereco)
+  }
+  public atulizaNumero(numero:string):void{
+    this.numero = numero
+    console.log(this.numero)
+
+  }
+  public atulizaComplemento(complemento: string): void{
+    this.complemento = complemento
+    console.log(this.complemento)
+
+  }
+  public atualizaFormaPagamento(formaPagamento:string):void{
+    this.formaPagamento = formaPagamento
+    console.log(this.formaPagamento)
+
   }
 
 }
