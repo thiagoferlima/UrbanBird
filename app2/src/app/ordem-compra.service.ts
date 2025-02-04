4fe84d0aed18d555d9129e1f28b8cd62cb49d990
thiago-fernandes <thicodes.com>
2025-02-04 09:19:36 -0300

    njetando o serviço HTTP OrdemCompraService

--- app2/src/app/ordem-compra.service.ts
+++ app2/src/app/ordem-compra.service.ts
@@ -1,5 +1,8 @@
 import { Pedido } from "./shared/pedido.model"
+import { Injectable } from "@angular/core"
+import { Http } from "@angular/http"
 export class OrdemCompraService{
+    constructor(private http: Http){}
     public efetivarCompra(pedido:Pedido): void{
         console.log(pedido)
     }
