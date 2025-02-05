35796dea2d011efdbcd04ada1555ee6c4b2fe7f1
thiago-fernandes <thicodes.com>
2025-02-05 11:21:53 -0300

    Enviando o formulário via requisição HTTP (POST)

--- app2/src/app/ordem-compra/ordem-compra.component.ts
+++ app2/src/app/ordem-compra/ordem-compra.component.ts
@@ -18,7 +18,16 @@ export class OrdemCompraComponent implements OnInit {
     
   }
   public confirmarCompra(): void{
-    console.log(this.formulario)
-
+    
+    let pedido: Pedido = new Pedido(
+      this.formulario.value.endereco,
+      this.formulario.value.numero,
+      this.formulario.value.complemento,
+      this.formulario.value.formaPagamento
+    )
+    this.ordemCompraService.efetivarCompra(pedido)
+    .subscribe((idPedido: number)=> {
+      console.log('Pedido cadastrado com sucesso!!! ID do Pedido' + idPedido)
+    })
   }
 }
