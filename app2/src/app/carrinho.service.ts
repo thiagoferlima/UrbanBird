880b287ff7c13924528941aa5838e6c665a9f6bd
thiago-fernandes <thicodes.com>
2025-02-06 20:06:04 -0300

    Diminuindo a quantidade dos itens

--- app2/src/app/carrinho.service.ts
+++ app2/src/app/carrinho.service.ts
@@ -46,6 +46,13 @@ class CarrinhoService{
             itemCarrinhoEncontrado.quantidade += 1
         }
     }
+    public diminuirQuantidade(itemCarrinho: ItemCarrinho): void{
+        console.log(itemCarrinho)
+        let itemCarrinhoEncontrado = this.itens.find((item: ItemCarrinho) => item.id === itemCarrinho.id)
+        if(itemCarrinhoEncontrado){
+            itemCarrinhoEncontrado.quantidade -= 1
+        }
+    }
 
 }
 export {CarrinhoService}
